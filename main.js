c9ed18ca8db4c483eb87b2349f4cc06e3fb77f11
mahdtomar <72965518+mahdtomar@users.noreply.github.com>
2022-02-09 12:41:36 +0200

    improving the selection menu
    - function to highlight it
    - css classes to change the styling for the   selected offer
    - html ids and classes to help with js

--- main.js
+++ main.js
@@ -4,20 +4,26 @@ let body = document.body;
 let backScreen = document.createElement("div");
 let  BS= document.createTextNode("");
 let packages = selectionMenu.children
+let bsp = document.getElementById("bsp")
+let bes = document.getElementById("bes");
+let mse = document.getElementById("mse");
+let norm = document.getElementById("norm");
+let myRadios = [bsp, bes, mse, norm];
 
-// HTML - ADDING ELEMENTS 
+// HTML
 backScreen.appendChild(BS)
+backScreen.setAttribute("onclick", "closemenu()");
+closeIcon.setAttribute("onclick", "closemenu()");
 body.append(backScreen)
 
-closeIcon.addEventListener("click",() =>{
+function closemenu() {
   // HTML
   // CSS
   backScreen.classList.remove("black-screen")
   selectionMenu.style.display = "none";
   selectionMenu.classList.remove("newOffer");
   document.body.style.overflow = "visible";
-  
-})
+}
 
 function selectOne() {
   // HTML
@@ -26,6 +32,19 @@ function selectOne() {
   selectionMenu.classList.add("newOffer");
   document.body.style.overflow = "hidden";
   backScreen.classList.add("black-screen")
-  
 }
 
+function highlight(e) {
+  for(i=0;i<myRadios.length;i++){
+    if (myRadios[i] == e) {
+      myRadios[i].parentElement.classList.add("selected")
+      i<3? myRadios[i].parentElement.children[4].style.display = "block":"";
+      i<3? myRadios[i].parentElement.children[4].style.maxHeight = "300px":"";
+      // console.log(myRadios[i])
+    }else{
+      myRadios[i].parentElement.classList.remove("selected");
+      i<3?myRadios[i].parentElement.children[4].style.display = "none":""
+      i<3?myRadios[i].parentElement.children[4].style.maxHeight = "none":""
+    }
+  }
+}
